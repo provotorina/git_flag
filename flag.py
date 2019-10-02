469493dfc940bb70fb3ff6429208a9730adb8bfb
provotorina_pycharm <provotorina@yandex.ru>
2019-10-02 23:03:42 +0300

    добавлен флагшток

---
+++ flag.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+screen.fill((150, 150, 150))
+
+def draw():
+    width = 0
+    color = (0, 0, 0)
+    point = [(40,40), (5,420)]
+    pygame.draw.rect(screen, color, point, width)
+
+draw()
+
+while pygame.event.wait().type != pygame.QUIT:
+    pygame.display.flip()
+
+pygame.quit()
